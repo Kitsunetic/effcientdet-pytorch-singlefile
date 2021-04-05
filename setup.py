4b360bea6d7961df43ab85d46f27a122a4640414
Kitsunetic <shenhaichenhai@gmail.com>
2021-04-05 19:46:49 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="effcientdet-pytorch-singlefile",
-    version="0.0.1",
+    name="effdet_torch_singlefile",
+    version="0.0.2",
     url="https://github.com/Kitsunetic/effcientdet-pytorch-singlefile",
     license="MIT",
     author="Kitsunetic",
     author_email="1996.jh.shim@gmail.com",
-    description="effcientdet-pytorch-singlefile",
+    description="effdet_torch_singlefile",
     packages=find_packages(),
     long_description=open("README.md").read(),
     zip_safe=False,
